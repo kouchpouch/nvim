44e3968532ba688e367641acd9809c77a2a1d6fd
kouchpouch <nakouchdoge@gmail.com>
2026-04-06 13:34:31 -0500

    removed unused themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,9 +1,12 @@
 return {
-	{ "neanias/everforest-nvim", style = "hard"},
-	{ "rose-pine/neovim", name = "rose-pine" },
-	{ "catppuccin/nvim", name = "catppuccin"},
+--	{ "neanias/everforest-nvim", style = "hard"},
+--	{ "rose-pine/neovim", name = "rose-pine" },
+--	{ "catppuccin/nvim", name = "catppuccin"},
+--	{ "wtfox/jellybeans.nvim", name = "jellybeans", lazy = false, opts = {}, },
 	{ "miikanissi/modus-themes.nvim" },
 	{ "bluz71/vim-moonfly-colors", name = "moonfly" },
     { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false },
-	{ "EdenEast/nightfox.nvim" }
+	{ "folke/tokyonight.nvim", lazy = false, opts = {}, },
+--	{ "EdenEast/nightfox.nvim" },
+	{ "Mofiqul/vscode.nvim" }
 }
